c02e098bfcdd6df55d422445252285e2f6fe6f30
serj <zenitovic@gmail.com>
2024-10-16 16:30:13 +0300

    add icons for navigation

--- src/js/pages/index.js
+++ src/js/pages/index.js
@@ -28,8 +28,16 @@ class Main {
               <div class="section-view-navigate">
                 <div class="swiper-container swiper-thumbs">
                   <div class="swiper-wrapper" preview-list=""></div>
-                  <button type="button" class="swiper-button-prev"></button>
-                  <button type="button" class="swiper-button-next"></button>
+                  <button type="button" class="swiper-button-prev">
+                    <svg width="12" height="19" viewBox="0 0 12 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M10 17.5L2 9.5L10 1.5" stroke="currentColor" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                  </button>
+                  <button type="button" class="swiper-button-next">
+                    <svg width="12" height="19" viewBox="0 0 12 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                      <path d="M2 17.5L10 9.5L2 1.5" stroke="currentColor" stroke-width="2.66667" stroke-linecap="round" stroke-linejoin="round"/>
+                    </svg>
+                  </button>
                 </div>
               </div>
             </div>
